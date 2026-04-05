173757aaafac577887f10b077a90228e356291e3
Gerrard <gyee005ntu@gmail.com>
2026-04-06 03:53:24 +0800

    added player

---
+++ game/combatant/Player.java
@@ -0,0 +1,24 @@
+package game.combatant;
+
+public abstract class Player extends Combatant {
+    private final List<Item> inventory = new ArrayList<>();
+    private int specialSkillCooldown = 0;
+
+    public Player(String name, int maxHP, int attack, int defense, int speed) {
+        super(name, maxHP, attack, defense, speed);
+    }
+
+    public List<Item> getInventory() { return inventory; }
+    public int getSpecialSkillCooldown() { return specialSkillCooldown; }
+
+    public void addItem(Item item) { inventory.add(item); }
+    public void removeItem(Item item) { inventory.remove(item); }
+
+    public void startCooldown(int turns) { specialSkillCooldown = turns; }
+
+    public void reduceCooldown() {
+        if (specialSkillCooldown > 0) specialSkillCooldown--;
+    }
+
+    public boolean isSpecialSkillReady() { return specialSkillCooldown <= 0; }
+}
