0c72d2db653b874f9fbd4579bcabb4bc2549d3ec
Gerrard <gyee005ntu@gmail.com>
2026-04-07 15:16:42 +0800

    Edited Wolf class

--- game/combatant/Wolf.java
+++ game/combatant/Wolf.java
@@ -2,12 +2,7 @@ package game.combatant;
 
 public class Wolf extends Enemy {
 
-    public Wolf() {
-        super("Wolf", 50, 18, 4, 20);
-    }
-
-    @Override
-    public void basicAttack(Combatant target) {
-        target.takeDamage(getAttack());
+    public Wolf(String name, EnemyActionStrategy strategy) {
+        super(name, 40, 45, 5, 35, strategy);
     }
 }
