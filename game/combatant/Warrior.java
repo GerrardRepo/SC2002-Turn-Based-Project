68aeabdf202c076f090a40ae1935eafbe4a15f18
Gerrard <gyee005ntu@gmail.com>
2026-04-07 15:18:16 +0800

    Modified warrior class for low coupling

--- game/combatant/Warrior.java
+++ game/combatant/Warrior.java
@@ -2,17 +2,7 @@ package game.combatant;
 
 public class Warrior extends Player {
 
-    public Warrior(String name) {
-        super(name, 120, 20, 15, 10); // HP, attack, defense, speed
-    }
-
-    // Shield Bash
-    public void shieldBash(Combatant target) {
-        target.addStatusEffect(new StunEffect(2)); // period for 2 round
-    }
-
-    @Override
-    public void basicAttack(Combatant target) {
-        target.takeDamage(getAttack());
+    public Warrior() {
+        super("Warrior", 260, 40, 20, 30);
     }
 }
