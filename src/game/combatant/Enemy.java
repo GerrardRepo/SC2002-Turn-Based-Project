6c01fb2fc4daf83bcdc83c6c9f17652ed772c650
Gerrard <gyee005ntu@gmail.com>
2026-04-12 09:50:21 +0800

    edited enemy

--- src/game/combatant/Enemy.java
+++ src/game/combatant/Enemy.java
@@ -1,12 +1,16 @@
 package game.combatant;
 
+import game.strategy.EnemyActionStrategy;
+
 public abstract class Enemy extends Combatant {
-    private final EnemyActionStrategy actionStrategy;
+    private EnemyActionStrategy actionStrategy;
 
     public Enemy(String name, int maxHP, int attack, int defense, int speed, EnemyActionStrategy strategy) {
         super(name, maxHP, attack, defense, speed);
         this.actionStrategy = strategy;
     }
 
-    public EnemyActionStrategy getActionStrategy() { return actionStrategy; }
+    public EnemyActionStrategy getActionStrategy() {
+        return actionStrategy;
+    }
 }
