f680a1fb4d56ac3c3e0336f328b6a5e9a25d6275
Gerrard <gyee005ntu@gmail.com>
2026-04-12 14:09:20 +0800

    added comments for wizard

--- src/game/combatant/Wizard.java
+++ src/game/combatant/Wizard.java
@@ -1,11 +1,13 @@
 package game.combatant;
 
+// HP:200 ATK:50 DEF:10 SPD:20 
 public class Wizard extends Player {
     public Wizard() {
         super("Wizard", 200, 50, 10, 20);
     }
 
-    public void addArcaneBlastBonus(int bonus) {
-        setAttack(getAttack() + bonus);
+    // +10 ATK per enemy killed by arcane blast (permanent for the battle)
+    public void addArcaneBlastBonus(int bonus){
+        setAttack( getAttack() + bonus );
     }
 }
