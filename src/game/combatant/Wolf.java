b5467c0c215cd78391d8a1dac2d2095be47d1c44
Gerrard <gyee005ntu@gmail.com>
2026-04-12 14:10:00 +0800

    Added comments

--- src/game/combatant/Wolf.java
+++ src/game/combatant/Wolf.java
@@ -2,6 +2,7 @@ package game.combatant;
 
 import game.strategy.EnemyActionStrategy;
 
+// HP:40 ATK:45 DEF:5 SPD:35 
 public class Wolf extends Enemy {
     public Wolf(String name, EnemyActionStrategy strategy) {
         super(name, 40, 45, 5, 35, strategy);
