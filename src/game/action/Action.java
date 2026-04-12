b113720a1efee330f2a4ed74838365b37be26b3b
Gerrard <gyee005ntu@gmail.com>
2026-04-12 12:56:28 +0800

    Edited action

--- src/game/action/Action.java
+++ src/game/action/Action.java
@@ -1,4 +1,8 @@
 package game.action;
+
+import game.combatant.Combatant;
+import game.engine.BattleContext;
+
 public interface Action {
     void execute(Combatant actor, BattleContext context);
 }
