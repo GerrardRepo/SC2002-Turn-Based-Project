1b8b8ba398721a5515c1ea0a47759bddfae57e71
Gerrard <gyee005ntu@gmail.com>
2026-04-12 09:51:17 +0800

    edited goblin

--- src/game/combatant/Goblin.java
+++ src/game/combatant/Goblin.java
@@ -1,7 +1,8 @@
 package game.combatant;
 
-public class Goblin extends Enemy {
+import game.strategy.EnemyActionStrategy;
 
+public class Goblin extends Enemy {
     public Goblin(String name, EnemyActionStrategy strategy) {
         super(name, 55, 35, 15, 25, strategy);
     }
