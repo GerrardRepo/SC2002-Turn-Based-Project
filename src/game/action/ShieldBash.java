1d1fffada8645db88f51fb442fee37842b9ab4d2
Gerrard <gyee005ntu@gmail.com>
2026-04-12 09:33:00 +0800

    edited shield bash

--- src/game/action/ShieldBash.java
+++ src/game/action/ShieldBash.java
@@ -1,21 +1,51 @@
 package game.action;
 
 import game.combatant.Combatant;
+import game.combatant.Player;
+import game.engine.BattleContext;
 import game.statuseffect.StunEffect;
 
-public class ShieldBash implements Action
-{
-	@Override
-	public void execute(Combatant actor, BattleContext context)
-	{
-		Combatant target = context.selectTarget(actor);
-		
-		int damage = Math.max(0, actor.getAttack() - target.getDefense());
-		
-		target.takeDamage(damage);
-		
-		target.addStatusEffect(new StunEffect(2));
-		
-		context.log(actor.getName() + " uses Shield Bash on " + target.getName() + " dealing " + damage + " damage and stunning the target.");
-	}
+public class ShieldBash implements Action {
+    private Combatant target;
+    private boolean fromPowerStone;
+
+    public ShieldBash(Combatant target, boolean fromPowerStone) {
+        this.target = target;
+        this.fromPowerStone = fromPowerStone;
+    }
+
+    public ShieldBash(Combatant target) {
+        this.target = target;
+        this.fromPowerStone = false;
+    }
+
+    public void execute(Combatant actor, BattleContext context) {
+        int damage = actor.getAttack() - target.getDefense();
+        if (damage < 0) {
+            damage = 0;
+        }
+
+        int hpBefore = target.getCurrentHP();
+        target.takeDamage(damage);
+
+        System.out.println(actor.getName() + " -> Shield Bash -> " + target.getName()
+                + ": HP: " + hpBefore + " -> " + target.getCurrentHP()
+                + " (dmg: " + actor.getAttack() + "-" + target.getDefense() + "=" + damage + ")");
+
+        if (target.isAlive()) {
+            target.addStatusEffect(new StunEffect(2));
+            System.out.println("  " + target.getName() + " STUNNED (2 turns)");
+        } else {
+            System.out.println("  " + target.getName() + " has been ELIMINATED!");
+        }
+
+        // Power Stone uses don't trigger cooldown
+        if (!fromPowerStone) {
+            if (actor instanceof Player) {
+                Player player = (Player) actor;
+                player.startCooldown(3);
+                System.out.println("  Special Skills Cooldown set to 3 rounds");
+            }
+        }
+    }
 }
