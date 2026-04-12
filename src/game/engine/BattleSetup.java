ed0eb957e453d78622097387c3776cabebe6538e
Gerrard <gyee005ntu@gmail.com>
2026-04-12 09:19:51 +0800

    Added a setup to initialise game

---
+++ src/game/engine/BattleSetup.java
@@ -0,0 +1,67 @@
+package game.engine;
+
+import game.combatant.Enemy;
+import game.combatant.Goblin;
+import game.combatant.Player;
+import game.combatant.Wolf;
+import game.item.Item;
+import game.strategy.BasicAttackStrategy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BattleSetup {
+    private Player player;
+    private Level level;
+
+    public BattleSetup(Player player, Level level, List<Item> chosenItems) {
+        this.player = player;
+        this.level = level;
+
+        // Add chosen items to player inventory
+        for (int i = 0; i < chosenItems.size(); i++) {
+            player.addItem(chosenItems.get(i));
+        }
+    }
+
+    public Player getPlayer() { return player; }
+    public Level getLevel() { return level; }
+
+    public List<Enemy> createInitialEnemies() {
+        ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+        BasicAttackStrategy strategy = new BasicAttackStrategy();
+
+        if (level == Level.EASY) {
+            enemies.add(new Goblin("Goblin A", strategy));
+            enemies.add(new Goblin("Goblin B", strategy));
+            enemies.add(new Goblin("Goblin C", strategy));
+        } else if (level == Level.MEDIUM) {
+            enemies.add(new Goblin("Goblin", strategy));
+            enemies.add(new Wolf("Wolf", strategy));
+        } else if (level == Level.HARD) {
+            enemies.add(new Goblin("Goblin A", strategy));
+            enemies.add(new Goblin("Goblin B", strategy));
+        }
+
+        return enemies;
+    }
+
+    public List<Enemy> createBackupEnemies() {
+        ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+        BasicAttackStrategy strategy = new BasicAttackStrategy();
+
+        if (level == Level.MEDIUM) {
+            enemies.add(new Wolf("Wolf A", strategy));
+            enemies.add(new Wolf("Wolf B", strategy));
+        } else if (level == Level.HARD) {
+            enemies.add(new Goblin("Goblin C", strategy));
+            enemies.add(new Wolf("Wolf A", strategy));
+            enemies.add(new Wolf("Wolf B", strategy));
+        }
+
+        return enemies;
+    }
+
+    public boolean hasBackupSpawn() {
+        return level == Level.MEDIUM || level == Level.HARD;
+    }
+}
