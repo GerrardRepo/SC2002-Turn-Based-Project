806d2ae67c66ead4813b2dea32160c420b14eb1a
Gerrard <gyee005ntu@gmail.com>
2026-04-12 10:08:20 +0800

    Adjusted wolf so that it works

--- src/game/combatant/Wolf.java
+++ src/game/combatant/Wolf.java
@@ -1,7 +1,8 @@
 package game.combatant;
 
-public class Wolf extends Enemy {
+import game.strategy.EnemyActionStrategy;
 
+public class Wolf extends Enemy {
     public Wolf(String name, EnemyActionStrategy strategy) {
         super(name, 40, 45, 5, 35, strategy);
     }
