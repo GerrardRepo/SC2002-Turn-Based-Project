57f17dcc2ba4f1a54b7770f709fac74538c3c1a2
Gerrard <gyee005ntu@gmail.com>
2026-04-12 14:17:01 +0800

    fixed bugs

--- src/game/item/SmokeBomb.java
+++ src/game/item/SmokeBomb.java
@@ -4,8 +4,8 @@ import game.combatant.Player;
 import game.engine.BattleContext;
 import game.statuseffect.SmokeBombInvulnerability;
 
+// blocks all incoming damage for 2 rounds
 public class SmokeBomb implements Item {
-
     public void use(Player player, BattleContext context) {
         player.addStatusEffect(new SmokeBombInvulnerability(2));
         System.out.println("  Enemy attacks deal 0 damage this turn + next turn");
