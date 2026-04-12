369f93682d8d4d9ea64647b88c7e0d1348df29ca
Gerrard <gyee005ntu@gmail.com>
2026-04-12 14:19:11 +0800

    modified stun

--- src/game/statuseffect/StunEffect.java
+++ src/game/statuseffect/StunEffect.java
@@ -2,19 +2,16 @@ package game.statuseffect;
 
 import game.combatant.Combatant;
 
+// prevents the target from acting on their turn
 public class StunEffect extends StatusEffect {
 
     public StunEffect(int duration) {
         super(duration);
     }
 
-    public void applyTo(Combatant target) {
-        // Stun does not change any stats
-    }
+    public void applyTo(Combatant target) {}
 
-    public void removeFrom(Combatant target) {
-        // Nothing to undo
-    }
+    public void removeFrom(Combatant target) {}
 
     public boolean preventsAction() {
         return true;
